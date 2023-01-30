a72e97f25c43a7b73c3aa21b62bdff8aa7720f01
Oleg <gelosh82@gmail.com>
2023-01-30 09:05:54 +0300

    hw last

--- js/script.js
+++ js/script.js
@@ -131,9 +131,9 @@ window.addEventListener('DOMContentLoaded', function() {
     //Form
 
     let message = {
-        laoding: 'Loading...',
-        succees: 'Thank you we call you Soon!',
-        failure: 'Error, some thing wrong'
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
     };
 
     let form = document.querySelector('.main-form'),
@@ -141,15 +141,17 @@ window.addEventListener('DOMContentLoaded', function() {
         statusMessage = document.createElement('div');
 
         statusMessage.classList.add('status');
-        form.addEventListener('submit', function(event) {
+
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         form.appendChild(statusMessage);
 
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
         let formData = new FormData(form);
+
         let obj = {};
         formData.forEach(function(value, key) {
             obj[key] = value;
@@ -160,9 +162,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
         request.addEventListener('readystatechange', function() {
             if (request.readyState < 4) {
-                statusMessage.innerHTML = message.laoding;
-            } else if (request.readyState == 4 && request.status == 200) {
-                statusMessage.innerHTML = message.succees;
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
             } else {
                 statusMessage.innerHTML = message.failure;
             }
